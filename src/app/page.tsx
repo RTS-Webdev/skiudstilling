99a83c9b054fc5c6b1e4746ebd915a30c57fbb0e
Mikkel <mikkelelvers@outlook.com>
2024-12-10 12:37:15 +0100

    Added FadingImage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FadingImage } from "$/components/FadingImage";
 import Navigation from "$/components/Navigation";
 
 export default function Home() {
@@ -6,8 +7,12 @@ export default function Home() {
       <header className="fixed top-0 left-0 right-0 z-50">
         <Navigation />
       </header>
-      <main></main>
+      <main className="pt-[15vh]">
+        <section className="relative w-full h-screen">
+          <h2 className="text-center mb-4 font-semibold text-4xl">Headline</h2>
+          <FadingImage />
+        </section>
+      </main>
     </div>
   );
 }
-
---
+++ src/components/FadingImage.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+interface FadingImageProps {
+  interval?: number
+}
+
+const images = [
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+]
+
+export function FadingImage({interval = 5000 }: FadingImageProps) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, interval)
+
+    return () => clearInterval(timer)
+  }, [images.length, interval])
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.img
+        key={currentIndex}
+        src={images[currentIndex]}
+        alt={"Image " + currentIndex}
+        className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 h-4/5 object-cover"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+      />
+    </AnimatePresence>
+  )
+}
