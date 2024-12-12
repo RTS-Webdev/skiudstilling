4f9365f860211045959c28f8f7ed43a0d8558fba
Mikkel <mikkelelvers@outlook.com>
2024-12-12 08:22:17 +0100

    changed bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navigation from "$/components/Navigation";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-neutral-900 to-black text-white">
+    <div className="min-h-screen bg-gradient-to-b from-black to-[#100f0f] text-white">
       <header className="fixed top-0 left-0 right-0 z-50">
         <Navigation />
       </header>
